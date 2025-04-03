f4f0f7eb0b8a96d720caeabf9921cd3bd05d6ad3
Enggudit <engg.udit07@gmail.com>
2025-04-03 23:30:24 +0530

    Initial commit with backend and client directories

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
+const dotenv = require('dotenv');
 const express= require('express');
 const app= express();
+const cors = require('cors');
+dotenv.config();
+const connectTodb = require('./db/db');
 
+connectTodb();
+
+app.use(cors());
 
 app.get('/', (req,res) =>{
-    res.send("Backend runn success fully");
+    res.send("Backend running successfully");
 });
 
 module.exports =app;
---
+++ backend/routes/users.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const {valitionresult} = require("express-validator");
+
+
+router.post('./register',)
+
+
+module.exports - router;
