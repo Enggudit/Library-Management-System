6c350b853bd6859f0a4473fab099425b24709cf1
Enggudit <engg.udit07@gmail.com>
2025-04-04 08:45:49 +0530

    Initial commit

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {valitionresult} = require("express-validator");
 const userController = require('../controllers/users.controllers')
+const authMiddellware = require('../middlewares/auth.middlewares');
+
+
 
 router.post('./register',[
     body('email').isEmail().withMessage('Invalid Emaill/Password'),
@@ -13,5 +16,13 @@ router.post('./register',[
     userController.registerUser
 )
 
+router.post('/logins',[
+    body('email').isEmail().withMessage('Invalid Emaill/Password'),
+    body('fullname.firstname').isLength({ min: 3}).withMessage('first name must be at least 3 charachet long'),],
+
+    userController.loginUser
+)
+
+router.get('./profile',authMiddellware.authuser,  userController.getUserProfile)
 
 module.exports - router;
